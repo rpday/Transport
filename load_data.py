b10d7a798610204a86953b111f13a854ada80575
Ryan P Day <ryanday7@gmail.com>
2019-02-13 08:34:15 -0800

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -23,27 +23,27 @@ class data_obj:
     
     
     def load_data(self,fnm):
-        data = {'time':[],'T':[],'Vr':[],'Rs':[],'ImV':[],'ReV':[]}
+        data = {'time':[],'T':[],'Tr':[],'Vr':[],'Rs':[],'ImV':[],'ReV':[]}
         with open(fnm,'r') as fromf:
             for line in fromf:
                 data_line = line.split('|')
                 try:
                     tmp_data  = [float(t) for t in data_line]
-                except TypeError:
+                except ValueError:
                     continue
                 data['time'].append(tmp_data[0])
                 data['T'].append(tmp_data[1])
-                data['Vr'].append(tmp_data[2])
-                data['ReV'].append(tmp_data[3])
-                data['ImV'].append(tmp_data[4])
-                data['Rs'].append(tmp_data[5])
+                data['Tr'].append(tmp_data[2])
+                data['Vr'].append(tmp_data[3])
+                data['ReV'].append(tmp_data[4])
+                data['ImV'].append(tmp_data[5])
+                data['Rs'].append(tmp_data[6])
         return data    
 
 
 
 if __name__ == "__main__":
-    data_cool = data_obj('C://Users//ARPES08//Desktop//FeSe_cool')
+    data_cool = data_obj('C:/Users/rday/Downloads/Transport-master/Transport-master/FeSe_heat_03_11022019.txt')
     data_cool.__plot_data__('T','Rs')
     
                 
-        
